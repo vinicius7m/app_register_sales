c28a8758ab379473f9bc6e380809a620e1d2e9db
Vinicius Moreira <vinicius.vsm777@gmail.com>
2022-03-11 23:43:52 -0300

    Bootstrap added

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+    <script type="" src="../assets/bootstrap/js/bootstrap.bundle.min.js">
+</body>
+</html>
+
--- public/home.php
+++ public/home.php
@@ -2,5 +2,7 @@
 require_once '../includes/header.php';
 require_once '../classes/autoload.php';
 ?>
+<h1>Hello World!</h1>
+
 
 <?php require_once '../includes/footer.php' ?>
